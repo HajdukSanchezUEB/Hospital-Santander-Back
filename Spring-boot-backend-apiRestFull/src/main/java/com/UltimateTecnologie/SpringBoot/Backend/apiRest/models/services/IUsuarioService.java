5598bf41cebc7741f5514b3b0ca8b5b1fa83e899
JorgeDevOps-GitHub <81051210+JorgeDevOps-GitHub@users.noreply.github.com>
2021-06-09 16:07:30 -0500

    Delete Spring-boot-backend-apiRestFull directory

--- Spring-boot-backend-apiRestFull/src/main/java/com/UltimateTecnologie/SpringBoot/Backend/apiRest/models/services/IUsuarioService.java
+++
@@ -1,8 +0,0 @@
-package com.UltimateTecnologie.SpringBoot.Backend.apiRest.models.services;
-
-import com.UltimateTecnologie.SpringBoot.Backend.apiRest.models.entity.Usuario;
-
-public interface IUsuarioService {
-
-	public Usuario findByUsername(String username);
-}
